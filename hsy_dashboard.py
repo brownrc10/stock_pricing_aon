ddf4a7728147cae522266a4ed1db9b50206a695a
Ryan Brown <brown.7438@osu.edu>
2026-04-06 21:27:13 -0400

    adding files to start dashboard

---
+++ hsy_dashboard.py
@@ -0,0 +1,7 @@
+import datetime
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+import streamlit as st
+
+st.set_page_config(page_title="HSY Award Valuation", layout="wide")
